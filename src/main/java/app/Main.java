c03537bca3e680174ef9f3dbce3cc1edebf7a718
Lorenzo <lorenzo_racca@hotmail.com>
2022-10-12 10:54:54 +0200

    Removed useless Exception

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,11 +3,9 @@ package app;
 import com.formdev.flatlaf.FlatLightLaf;
 import gui.Controller;
 
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         FlatLightLaf.setup();
         Controller controller = Controller.getInstance();
         controller.showWindow();
