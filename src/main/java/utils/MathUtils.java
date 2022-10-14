ab1a20904886939fb56eb487a34db4808205edc0
Lorenzo <lorenzo_racca@hotmail.com>
2022-10-14 10:11:25 +0200

    MathUtils refactor

--- src/main/java/utils/MathUtils.java
+++ src/main/java/utils/MathUtils.java
@@ -61,34 +61,28 @@ public class MathUtils {
     }
 
     public static double cprod(DoublePoint p0, DoublePoint p1, DoublePoint p2, DoublePoint p3) {
-        double x1, y1, x2, y2;
-
-        x1 = p1.getX() - p0.getX();
-        y1 = p1.getY() - p0.getY();
-        x2 = p3.getX() - p2.getX();
-        y2 = p3.getY() - p2.getY();
+        double x1 = p1.getX() - p0.getX();
+        double y1 = p1.getY() - p0.getY();
+        double x2 = p3.getX() - p2.getX();
+        double y2 = p3.getY() - p2.getY();
 
         return x1 * y2 - x2 * y1;
     }
 
     public static double iprod(DoublePoint p0, DoublePoint p1, DoublePoint p2) {
-        double x1, y1, x2, y2;
-
-        x1 = p1.getX() - p0.getX();
-        y1 = p1.getY() - p0.getY();
-        x2 = p2.getX() - p0.getX();
-        y2 = p2.getY() - p0.getY();
+        double x1 = p1.getX() - p0.getX();
+        double y1 = p1.getY() - p0.getY();
+        double x2 = p2.getX() - p0.getX();
+        double y2 = p2.getY() - p0.getY();
 
         return x1*x2 + y1*y2;
     }
 
     public static double iprod1(DoublePoint p0, DoublePoint p1, DoublePoint p2, DoublePoint p3) {
-        double x1, y1, x2, y2;
-
-        x1 = p1.getX() - p0.getX();
-        y1 = p1.getY() - p0.getY();
-        x2 = p3.getX() - p2.getX();
-        y2 = p3.getY() - p2.getY();
+        double x1 = p1.getX() - p0.getX();
+        double y1 = p1.getY() - p0.getY();
+        double x2 = p3.getX() - p2.getX();
+        double y2 = p3.getY() - p2.getY();
 
         return x1 * x2 + y1 * y2;
     }
@@ -108,26 +102,24 @@ public class MathUtils {
     }
 
     public static double tangent(DoublePoint p0, DoublePoint p1, DoublePoint p2, DoublePoint p3, DoublePoint q0, DoublePoint q1) {
-        double A, B, C, a, b, c, d, s, r1, r2;
+        double cprodA = cprod(p0, p1, q0, q1);
+        double cprodB = cprod(p1, p2, q0, q1);
+        double cprodC = cprod(p2, p3, q0, q1);
 
-        A = cprod(p0, p1, q0, q1);
-        B = cprod(p1, p2, q0, q1);
-        C = cprod(p2, p3, q0, q1);
+        double a = cprodA - 2 * cprodB + cprodC;
+        double b = -2 * cprodA + 2 * cprodB;
+        double c = cprodA;
 
-        a = A - 2 * B + C;
-        b = -2 * A + 2 * B;
-        c = A;
-
-        d = b * b - 4 * a * c;
+        double d = b * b - 4 * a * c;
 
         if (a==0 || d<0) {
             return -1.0;
         }
 
-        s = Math.sqrt(d);
+        double s = Math.sqrt(d);
 
-        r1 = (-b + s) / (2 * a);
-        r2 = (-b - s) / (2 * a);
+        double r1 = (-b + s) / (2 * a);
+        double r2 = (-b - s) / (2 * a);
 
         if (r1 >= 0 && r1 <= 1) {
             return r1;
